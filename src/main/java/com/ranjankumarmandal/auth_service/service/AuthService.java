1b5efff75ba3090d28987d7fd18fb07cfba09466
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-04-05 18:31:28 +0530

    Distributed Auth Service: added signup service layer dev changes

--- src/main/java/com/ranjankumarmandal/auth_service/service/AuthService.java
+++ src/main/java/com/ranjankumarmandal/auth_service/service/AuthService.java
@@ -1,14 +1,26 @@
 package com.ranjankumarmandal.auth_service.service;
 
 import com.ranjankumarmandal.auth_service.dto.LoginRequest;
+import com.ranjankumarmandal.auth_service.entity.User;
+import com.ranjankumarmandal.auth_service.repository.UserRepository;
 import com.ranjankumarmandal.auth_service.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Autowired
     private AuthenticationManager authManager;
@@ -16,8 +28,20 @@ public class AuthService {
     @Autowired
     private JwtUtil jwtUtil;
 
-    public String login(LoginRequest request) {
+    public void signup(String username, String password) {
+        if (userRepository.findByUsername(username).isPresent()) {
+            throw new RuntimeException("User already exists");
+        }
 
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setRole("ROLE_USER");
+
+        userRepository.save(user);
+    }
+
+    public String login(LoginRequest request) {
         authManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getUsername(),
